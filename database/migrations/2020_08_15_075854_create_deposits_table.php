7355537a3874bf1039b9a647d66399ce2391601d
Karyna Chelpan <cheplan15@gmail.com>
2020-08-20 21:56:04 +0300

    migrate new table

--- database/migrations/2020_08_15_075854_create_deposits_table.php
+++ database/migrations/2020_08_15_075854_create_deposits_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDepositsTable extends Migration
-{
+class CreateDepositsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('deposits', function (Blueprint $table) {
-            
+
             $table->engine = 'InnoDB';
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
@@ -24,10 +23,10 @@ class CreateDepositsTable extends Migration
             $table->dateTime('created_at')->nullable();
             $table->dateTime('updated_at')->nullable();
             $table->dateTime('finished_at')->nullable();
-            
+
             $table->float('sum')->nullable();
             $table->enum('interest_rate', ['0', '1', '2']);
-            
+
             $table->index('user_id');
             $table->foreign('user_id')
                     ->references('id')->on('users')
@@ -41,8 +40,8 @@ class CreateDepositsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('deposits');
     }
+
 }
