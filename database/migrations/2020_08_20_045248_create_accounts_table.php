d56772fcb537c7f4853539e7c2ecb4bf9eb943e6
Karyna Chelpan <cheplan15@gmail.com>
2020-08-21 19:59:45 +0300

    migration deposits, roles, users

---
+++ database/migrations/2020_08_20_045248_create_accounts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->float('balance')->default(0);
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+
+            $table->index('user_id');
+            $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
